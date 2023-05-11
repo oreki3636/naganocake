c81c4954a681b1904c6d4299b74268fd56ca600c
oreki3636 <127170175+oreki3636@users.noreply.github.com>
2023-05-11 07:02:58 +0000

    [finish]ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  #会員側のルーティング
-  devise_for :admins
-  devise_for :customers
+  #顧客用
+  #URL/custmers/sign_in
+  #skip[]内不要なローティング削除
+  devise_for :customers,skip:[:passwords],controllers:{
+   registrations: "public/registrations",
+   sessions: 'public/sessions'
+  }
+  #管理者用
+  #URL/admin/sign_in
+  devise_for :admin,skip:[:registrations,:passwords],controllers:{
+    sessions: "admin/sessions"
+  }
+
+
   root to: 'public/homes#top'
   get 'homes/about' => 'public/homes#about'
   get 'items' => 'public/item#index'
@@ -10,7 +21,4 @@ Rails.application.routes.draw do
   get 'items' => 'items#index'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_for :users, controllers: {
-        sessions: 'users/sessions'
-  }
 end
